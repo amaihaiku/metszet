cc326f1358f06ccf5430c5bf095c87be15e823fe
amaihaiku <sandorherman1@gmail.com>
2026-09-18 13:49:27 +0000

    Fix base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,9 @@
-import tailwindcss from '@tailwindcss/vite'
-import react from '@vitejs/plugin-react'
+
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: './',
   plugins: [react(), tailwindcss()],
+  base: '/omniforecast/', // A repositoryd pontos neve perjelek közé téve!
 })
-
